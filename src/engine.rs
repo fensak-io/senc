0a7e2b28ba812cb8625731e318031768777e2562
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2023-11-16 15:02:30 -0600

    refactor: Cleanup use of unwrap to avoid panics
    Signed-off-by: Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>

--- src/engine.rs
+++ src/engine.rs
@@ -81,23 +81,21 @@ pub async fn run_js_and_write(ctx: &Context, req: &RunRequest) -> Result<()> {
     for d in out_data_vec {
         // TODO
         // collect the errors and return one big error instead of failing fast
-        if let Err(e) = write_data(ctx.out_dir.as_path(), &req.out_file_stem, &d) {
-            return Err(e);
-        }
+        write_data(ctx.out_dir.as_path(), &req.out_file_stem, &d)?;
     }
     return Ok(());
 }
 
 // Run the javascript or typescript file available at the given file path through the Deno runtime.
 async fn run_js(ctx: &Context, req: &RunRequest) -> Result<vec::Vec<OutData>> {
-    let mut js_runtime = new_runtime(ctx, req);
+    let mut js_runtime = new_runtime(ctx, req)?;
     let mod_id = load_main_module(&mut js_runtime, &req.in_file).await?;
-    let main_fn = load_main_fn(&mut js_runtime, mod_id).unwrap();
+    let main_fn = load_main_fn(&mut js_runtime, mod_id)?;
     let result = js_runtime.call_and_await(&main_fn).await?;
     return load_result(&mut js_runtime, result);
 }
 
-fn new_runtime(ctx: &Context, req: &RunRequest) -> JsRuntime {
+fn new_runtime(ctx: &Context, req: &RunRequest) -> Result<JsRuntime> {
     let opext = Extension {
         name: "opbuiltins",
         ops: Cow::Borrowed(&[
@@ -114,15 +112,15 @@ fn new_runtime(ctx: &Context, req: &RunRequest) -> JsRuntime {
         })),
         ..Default::default()
     };
-    let tmplext = load_templated_builtins(ctx, req).unwrap();
-    JsRuntime::new(RuntimeOptions {
+    let tmplext = load_templated_builtins(ctx, req)?;
+    Ok(JsRuntime::new(RuntimeOptions {
         module_loader: Some(Rc::new(module_loader::TsModuleLoader::new(
             ctx.node_modules_dir.clone(),
         ))),
         extensions: vec![opext, tmplext],
         startup_snapshot: Some(Snapshot::Static(RUNTIME_SNAPSHOT)),
         ..Default::default()
-    })
+    }))
 }
 
 async fn load_main_module(js_runtime: &mut JsRuntime, file_path: &str) -> Result<usize> {
@@ -130,22 +128,19 @@ async fn load_main_module(js_runtime: &mut JsRuntime, file_path: &str) -> Result
     let mod_id = js_runtime.load_main_module(&main_module, None).await?;
     let result = js_runtime.mod_evaluate(mod_id);
     js_runtime.run_event_loop(false).await?;
-    if let Err(e) = result.await? {
-        return Err(e);
-    }
+    result.await??;
     return Ok(mod_id);
 }
 
 fn load_main_fn(js_runtime: &mut JsRuntime, mod_id: usize) -> Result<v8::Global<v8::Function>> {
-    let ns = js_runtime.get_module_namespace(mod_id).unwrap();
+    let ns = js_runtime.get_module_namespace(mod_id)?;
     let mut scope = js_runtime.handle_scope();
     let main_fn_key = v8::String::new(&mut scope, "main").unwrap();
     let main_fn_local: v8::Local<v8::Function> = ns
         .open(&mut scope)
         .get(&mut scope, main_fn_key.into())
         .unwrap()
-        .try_into()
-        .unwrap();
+        .try_into()?;
     let main_fn = v8::Global::new(&mut scope, main_fn_local);
     return Ok(main_fn);
 }
@@ -161,17 +156,17 @@ fn load_result(
 
     // Determine if the raw JS object from the runtime is an out data list object, in which case
     // each element needs to be cycled and converted.
-    if result_is_senc_out_data_array(&mut scope, result_local) {
-        let result_arr: v8::Local<v8::Array> = result_local.try_into().unwrap();
+    if result_is_senc_out_data_array(&mut scope, result_local)? {
+        let result_arr: v8::Local<v8::Array> = result_local.try_into()?;
         let result_arr_raw: &v8::Array = result_arr.borrow();
         let sz = result_arr_raw.length();
         for i in 0..sz {
             let item = result_arr_raw.get_index(&mut scope, i).unwrap();
-            let single_out = load_one_result(&mut scope, item).unwrap();
+            let single_out = load_one_result(&mut scope, item)?;
             out.push(single_out);
         }
     } else {
-        let single_out = load_one_result(&mut scope, result_local).unwrap();
+        let single_out = load_one_result(&mut scope, result_local)?;
         out.push(single_out);
     }
 
@@ -188,14 +183,11 @@ fn load_one_result(
     let mut out_type = OutputType::JSON;
 
     // Determine if the raw JS object from the runtime is an out data object.
-    if result_is_senc_out_data(scope, result_local) {
-        let result_obj: v8::Local<v8::Object> = result_local.try_into().unwrap();
+    if result_is_senc_out_data(scope, result_local)? {
+        let result_obj: v8::Local<v8::Object> = result_local.try_into()?;
         let out_type_key: v8::Local<v8::Value> = v8::String::new(scope, "out_type").unwrap().into();
-        let out_type_local: v8::Local<v8::String> = result_obj
-            .get(scope, out_type_key)
-            .unwrap()
-            .try_into()
-            .unwrap();
+        let out_type_local: v8::Local<v8::String> =
+            result_obj.get(scope, out_type_key).unwrap().try_into()?;
         let out_type_str: &str = &out_type_local.to_rust_string_lossy(scope);
         match out_type_str {
             "yaml" => {
@@ -215,37 +207,27 @@ fn load_one_result(
                 "OutData object can only have at most one of out_path or out_ext set, but not both"
             ));
         } else if maybe_out_path.is_string() {
-            let out_path_local: v8::Local<v8::String> = maybe_out_path.try_into().unwrap();
+            let out_path_local: v8::Local<v8::String> = maybe_out_path.try_into()?;
             out_path = Some(out_path_local.to_rust_string_lossy(scope));
             out_ext = None;
         } else if maybe_out_ext.is_string() {
-            let out_ext_local: v8::Local<v8::String> = maybe_out_ext.try_into().unwrap();
+            let out_ext_local: v8::Local<v8::String> = maybe_out_ext.try_into()?;
             out_ext = Some(out_ext_local.to_rust_string_lossy(scope));
             out_path = None;
         }
 
         let out_data_key: v8::Local<v8::Value> = v8::String::new(scope, "data").unwrap().into();
-        result_local = result_obj
-            .get(scope, out_data_key)
-            .unwrap()
-            .try_into()
-            .unwrap();
+        result_local = result_obj.get(scope, out_data_key).unwrap().try_into()?;
     }
 
     let data = match out_type {
         OutputType::JSON => {
-            let deserialized_result =
-                serde_v8::from_v8::<serde_json::Value>(scope, result_local).unwrap();
-            serde_json::to_string(&deserialized_result)
-                .unwrap()
-                .to_string()
+            let deserialized_result = serde_v8::from_v8::<serde_json::Value>(scope, result_local)?;
+            serde_json::to_string(&deserialized_result)?.to_string()
         }
         OutputType::YAML => {
-            let deserialized_result =
-                serde_v8::from_v8::<serde_yaml::Value>(scope, result_local).unwrap();
-            serde_yaml::to_string(&deserialized_result)
-                .unwrap()
-                .to_string()
+            let deserialized_result = serde_v8::from_v8::<serde_yaml::Value>(scope, result_local)?;
+            serde_yaml::to_string(&deserialized_result)?.to_string()
         }
     };
     return Ok(OutData {
@@ -258,31 +240,32 @@ fn load_one_result(
 fn result_is_senc_out_data(
     scope: &mut v8::HandleScope,
     result_local: v8::Local<v8::Value>,
-) -> bool {
+) -> Result<bool> {
     if !result_local.is_object() {
-        return false;
+        return Ok(false);
     }
 
-    let result_obj: v8::Local<v8::Object> = result_local.try_into().unwrap();
-    let is_senc_out_data_key: v8::Local<v8::Value> =
-        v8::String::new(scope, "__is_senc_out_data").unwrap().into();
-    return result_obj.has(scope, is_senc_out_data_key).unwrap();
+    let result_obj: v8::Local<v8::Object> = result_local.try_into()?;
+    let is_senc_out_data_key: v8::Local<v8::Value> = v8::String::new(scope, "__is_senc_out_data")
+        .unwrap()
+        .try_into()?;
+    return Ok(result_obj.has(scope, is_senc_out_data_key).unwrap());
 }
 
 fn result_is_senc_out_data_array(
     scope: &mut v8::HandleScope,
     result_local: v8::Local<v8::Value>,
-) -> bool {
+) -> Result<bool> {
     if !result_local.is_array() {
-        return false;
+        return Ok(false);
     }
 
-    let result_arr: v8::Local<v8::Array> = result_local.try_into().unwrap();
+    let result_arr: v8::Local<v8::Array> = result_local.try_into()?;
     let is_senc_out_data_array_key: v8::Local<v8::Value> =
         v8::String::new(scope, "__is_senc_out_data_array")
             .unwrap()
-            .into();
-    return result_arr.has(scope, is_senc_out_data_array_key).unwrap();
+            .try_into()?;
+    return Ok(result_arr.has(scope, is_senc_out_data_array_key).unwrap());
 }
 
 fn write_data(out_dir: &path::Path, out_file_stem: &str, data: &OutData) -> Result<()> {
@@ -299,9 +282,7 @@ fn write_data(out_dir: &path::Path, out_file_stem: &str, data: &OutData) -> Resu
         out_file_path_str.push_str(&data.out_ext.clone().unwrap());
     }
     let out_file_path = path_clean::clean(path::PathBuf::from(out_file_path_str));
-    if let Err(e) = files::assert_file_path_in_projectroot(&out_file_path, out_dir) {
-        return Err(e);
-    }
+    files::assert_file_path_in_projectroot(&out_file_path, out_dir)?;
 
     let out_file_dir = out_file_path.parent().unwrap();
     fs::create_dir_all(out_file_dir)?;
@@ -359,8 +340,8 @@ mod tests {
 
     #[tokio::test]
     async fn test_engine_runs_js() {
-        let expected_output: serde_json::Value =
-            serde_json::from_str(EXPECTED_SIMPLE_OUTPUT_JSON).unwrap();
+        let expected_output: serde_json::Value = serde_json::from_str(EXPECTED_SIMPLE_OUTPUT_JSON)
+            .expect("error unpacking simple expected output json");
 
         let p = get_fixture_path("simple.js");
         let req = RunRequest {
@@ -375,14 +356,15 @@ mod tests {
 
         assert_eq!(od.out_path, None);
         assert_eq!(od.out_ext, Some(String::from(".json")));
-        let actual_output: serde_json::Value = serde_json::from_str(&od.data).unwrap();
+        let actual_output: serde_json::Value =
+            serde_json::from_str(&od.data).expect("error unpacking js data");
         assert_eq!(actual_output, expected_output);
     }
 
     #[tokio::test]
     async fn test_engine_runs_ts() {
-        let expected_output: serde_json::Value =
-            serde_json::from_str(EXPECTED_SIMPLE_OUTPUT_JSON).unwrap();
+        let expected_output: serde_json::Value = serde_json::from_str(EXPECTED_SIMPLE_OUTPUT_JSON)
+            .expect("error unpacking simple expected output json");
 
         let p = get_fixture_path("simple.ts");
         let req = RunRequest {
@@ -397,14 +379,15 @@ mod tests {
 
         assert_eq!(od.out_path, None);
         assert_eq!(od.out_ext, Some(String::from(".json")));
-        let actual_output: serde_json::Value = serde_json::from_str(&od.data).unwrap();
+        let actual_output: serde_json::Value =
+            serde_json::from_str(&od.data).expect("error unpacking js data");
         assert_eq!(actual_output, expected_output);
     }
 
     #[tokio::test]
     async fn test_engine_runs_code_with_node_modules() {
-        let expected_output: serde_json::Value =
-            serde_json::from_str(EXPECTED_LODASH_OUTPUT_JSON).unwrap();
+        let expected_output: serde_json::Value = serde_json::from_str(EXPECTED_LODASH_OUTPUT_JSON)
+            .expect("error unpacking lodash expected output json");
 
         let p = get_fixture_path("with_lodash.js");
         let req = RunRequest {
@@ -419,14 +402,15 @@ mod tests {
 
         assert_eq!(od.out_path, None);
         assert_eq!(od.out_ext, Some(String::from(".json")));
-        let actual_output: serde_json::Value = serde_json::from_str(&od.data).unwrap();
+        let actual_output: serde_json::Value =
+            serde_json::from_str(&od.data).expect("error unpacking js data");
         assert_eq!(actual_output, expected_output);
     }
 
     #[tokio::test]
     async fn test_engine_runs_code_with_builtin_filefunctions() {
-        let expected_output: serde_json::Value =
-            serde_json::from_str(EXPECTED_RELPATH_OUTPUT_JSON).unwrap();
+        let expected_output: serde_json::Value = serde_json::from_str(EXPECTED_RELPATH_OUTPUT_JSON)
+            .expect("error unpacking relpath output json");
 
         let p = get_fixture_path("aws/us-east-1/vpc/main.js");
         let req = RunRequest {
@@ -441,14 +425,15 @@ mod tests {
 
         assert_eq!(od.out_path, None);
         assert_eq!(od.out_ext, Some(String::from(".json")));
-        let actual_output: serde_json::Value = serde_json::from_str(&od.data).unwrap();
+        let actual_output: serde_json::Value =
+            serde_json::from_str(&od.data).expect("error unpacking js data");
         assert_eq!(actual_output, expected_output);
     }
 
     #[tokio::test]
     async fn test_engine_runs_code_with_out_data_output() {
-        let expected_output: serde_json::Value =
-            serde_json::from_str(EXPECTED_OUTFILE_OUTPUT_JSON).unwrap();
+        let expected_output: serde_json::Value = serde_json::from_str(EXPECTED_OUTFILE_OUTPUT_JSON)
+            .expect("error unpacking outfile expected output json");
 
         let p = get_fixture_path("outfile.js");
         let req = RunRequest {
@@ -463,7 +448,8 @@ mod tests {
 
         assert_eq!(od.out_path, None);
         assert_eq!(od.out_ext, Some(String::from(".yml")));
-        let actual_output: serde_json::Value = serde_yaml::from_str(&od.data).unwrap();
+        let actual_output: serde_json::Value =
+            serde_yaml::from_str(&od.data).expect("error unpacking js data");
         assert_eq!(actual_output, expected_output);
     }
 
@@ -480,8 +466,8 @@ mod tests {
 
     #[tokio::test]
     async fn test_engine_runs_code_with_out_data_list_output() {
-        let expected_output: serde_json::Value =
-            serde_json::from_str(EXPECTED_OUTFILE_OUTPUT_JSON).unwrap();
+        let expected_output: serde_json::Value = serde_json::from_str(EXPECTED_OUTFILE_OUTPUT_JSON)
+            .expect("error unpacking outfile expected output json");
 
         let p = get_fixture_path("multi_outfile.js");
         let req = RunRequest {
@@ -496,13 +482,15 @@ mod tests {
         let d1 = &od_vec[0];
         assert_eq!(d1.out_path, None);
         assert_eq!(d1.out_ext, Some(String::from(".yml")));
-        let actual_output1: serde_json::Value = serde_yaml::from_str(&d1.data).unwrap();
+        let actual_output1: serde_json::Value =
+            serde_yaml::from_str(&d1.data).expect("error unpacking js data");
         assert_eq!(actual_output1, expected_output);
 
         let d2 = &od_vec[1];
         assert_eq!(d2.out_path, None);
         assert_eq!(d2.out_ext, Some(String::from(".json")));
-        let actual_output2: serde_json::Value = serde_json::from_str(&d2.data).unwrap();
+        let actual_output2: serde_json::Value =
+            serde_json::from_str(&d2.data).expect("error unpacking js data");
         assert_eq!(actual_output2, expected_output);
     }
 
