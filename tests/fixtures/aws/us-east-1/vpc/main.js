8490fd6fec780225cb51b9ab00b795d50eac8300
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2023-11-17 09:46:32 -0600

    feat: Rename path.relpath to path.rel to avoid stuttering
    Signed-off-by: Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>

--- tests/fixtures/aws/us-east-1/vpc/main.js
+++ tests/fixtures/aws/us-east-1/vpc/main.js
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: MPL-2.0
 
 export function main() {
-  const state = `${path.relpath(__projectroot, __dirname)}/terraform.tfstate`;
-  const mainf = path.relpath(__projectroot, __filename);
+  const state = `${path.rel(__projectroot, __dirname)}/terraform.tfstate`;
+  const mainf = path.rel(__projectroot, __filename);
   return { state, mainf };
 }
